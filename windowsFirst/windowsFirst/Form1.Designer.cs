ea8565a6b0ba4b39e09c559479269e1804419f00
neriman00 <47786630+neriman00@users.noreply.github.com>
2019-04-25 21:55:49 +0400

    Add files via upload

---
+++ windowsFirst/windowsFirst/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace windowsFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.lblfirstname = new System.Windows.Forms.Label();
+            this.lbllastname = new System.Windows.Forms.Label();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblwelcome = new System.Windows.Forms.Label();
+            this.cmbgourp = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.group1 = new System.Windows.Forms.Button();
+            this.txtgroup = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfirstname.Location = new System.Drawing.Point(16, 76);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(196, 30);
+            this.txtfirstname.TabIndex = 0;
+            // 
+            // lblfirstname
+            // 
+            this.lblfirstname.AutoSize = true;
+            this.lblfirstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfirstname.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblfirstname.Location = new System.Drawing.Point(28, 25);
+            this.lblfirstname.Name = "lblfirstname";
+            this.lblfirstname.Size = new System.Drawing.Size(102, 26);
+            this.lblfirstname.TabIndex = 1;
+            this.lblfirstname.Text = "firstname";
+            // 
+            // lbllastname
+            // 
+            this.lbllastname.AutoSize = true;
+            this.lbllastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllastname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbllastname.Location = new System.Drawing.Point(278, 25);
+            this.lbllastname.Name = "lbllastname";
+            this.lbllastname.Size = new System.Drawing.Size(101, 26);
+            this.lbllastname.TabIndex = 3;
+            this.lbllastname.Text = "lastname";
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlastname.Location = new System.Drawing.Point(283, 75);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(205, 30);
+            this.txtlastname.TabIndex = 2;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.Location = new System.Drawing.Point(16, 158);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(196, 76);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "login";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.Btnlogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(479, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 26);
+            this.label2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(632, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 26);
+            this.label4.TabIndex = 8;
+            // 
+            // lblwelcome
+            // 
+            this.lblwelcome.AutoSize = true;
+            this.lblwelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwelcome.Location = new System.Drawing.Point(1156, 35);
+            this.lblwelcome.Name = "lblwelcome";
+            this.lblwelcome.Size = new System.Drawing.Size(70, 26);
+            this.lblwelcome.TabIndex = 9;
+            this.lblwelcome.Text = "label5";
+            // 
+            // cmbgourp
+            // 
+            this.cmbgourp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbgourp.FormattingEnabled = true;
+            this.cmbgourp.Location = new System.Drawing.Point(881, 77);
+            this.cmbgourp.Name = "cmbgourp";
+            this.cmbgourp.Size = new System.Drawing.Size(187, 32);
+            this.cmbgourp.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(575, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 26);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "group desugn";
+            // 
+            // group1
+            // 
+            this.group1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.group1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.group1.Location = new System.Drawing.Point(570, 158);
+            this.group1.Name = "group1";
+            this.group1.Size = new System.Drawing.Size(205, 76);
+            this.group1.TabIndex = 12;
+            this.group1.Text = "Addgruop";
+            this.group1.UseVisualStyleBackColor = false;
+            this.group1.Click += new System.EventHandler(this.Group1_Click);
+            // 
+            // txtgroup
+            // 
+            this.txtgroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtgroup.Location = new System.Drawing.Point(570, 76);
+            this.txtgroup.Name = "txtgroup";
+            this.txtgroup.Size = new System.Drawing.Size(205, 30);
+            this.txtgroup.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(283, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 76);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "password";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(349, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 26);
+            this.label3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1260, 654);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtgroup);
+            this.Controls.Add(this.group1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbgourp);
+            this.Controls.Add(this.lblwelcome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.lbllastname);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.lblfirstname);
+            this.Controls.Add(this.txtfirstname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.Label lblfirstname;
+        private System.Windows.Forms.Label lbllastname;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblwelcome;
+        private System.Windows.Forms.ComboBox cmbgourp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button group1;
+        private System.Windows.Forms.TextBox txtgroup;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ windowsFirst/windowsFirst/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsFirst
+{
+    public partial class Form1 : Form
+    {
+        List<string> classes;
+        public Form1()
+        {
+            classes = new List<string>();
+            InitializeComponent();
+        }
+        private void Btnlogin_Click(object sender, EventArgs e)
+        {
+            string firstname = txtfirstname.Text;
+            string lastname = txtlastname.Text;
+
+            lblwelcome.Text ="xos gelmisiz " + firstname + " " + lastname;
+            lblwelcome.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblwelcome.Visible = false;
+            cmbgourp.Items.Add("K30");
+            cmbgourp.Items.Add("K10");
+            cmbgourp.Items.Add("C10");
+            cmbgourp.Items.Add("C20");
+            
+        }
+        private void Group1_Click(object sender, EventArgs e)
+        {
+            string textGroupName = txtgroup.Text;
+            classes.Add(textGroupName);
+            neriman();
+        }
+        public void neriman()
+        {
+            cmbgourp.Items.Clear();
+            cmbgourp.Items.AddRange(classes.ToArray());
+
+        }
+    }
+}
